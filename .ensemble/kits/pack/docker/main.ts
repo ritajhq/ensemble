75303c1024a30ea5d688e0a0b5d33cb97de56487
duesabati <ali.sbtan96@gmail.com>
2026-08-30 09:03:05 +0200

    fix(kits/pack/docker): restore build cache

--- .ensemble/kits/pack/docker/main.ts
+++ .ensemble/kits/pack/docker/main.ts
@@ -38,7 +38,6 @@ if (format.startsWith("image")) {
 }
 
 const result = await $`docker buildx build
-  --no-cache
   --tag ${ctx.outputName}
   --build-context packages=${ctx.packages}
   --build-context artifacts=${ctx.artifacts}
